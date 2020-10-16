7d461d66bdf2bb0f2f4c9e93dfe11aa001b47af8
Flávio Félix <fau_felix30@hotmail.com>
2020-10-16 15:33:50 -0300

    Começando a configuração do servidor e os dados dinâmicos

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -6,25 +6,45 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 //create icon
 const icon = L.icon({
-  iconUrl: "./public/images/map-marker.svg",
+  iconUrl: "/images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
   popupAnchor: [170, 2]
 })
 
-//create popup overlay
-const popup = L.popup({
-  closeButton: false,
-  className: 'map-popup',
-  minWidth: 240,
-  minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"></a>')
+
+function addMarker({id, name, lat, lng}){
+  
+  //create popup overlay
+  const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+  }).setContent(`${name} <a href="/orphanage?id=${id}"><img src="/images/arrow-white.svg"></a>`)
 
 
-//create and add maker
-L
-.marker([-27.2058128,-49.6932569], { icon })
-.addTo(map)
-.bindPopup(popup)
+  //create and add maker
+  L
+  .marker([lat,lng], { icon })
+  .addTo(map)
+  .bindPopup(popup)
 
+
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+
+orphanagesSpan.forEach( span => {
+
+  const orphanage = {
+    
+    id: span.dataset.id,
+    name: span.dataset.name,
+    lat: span.dataset.lat,
+    lng: span.dataset.lng
+  }
+
+  addMarker(orphanage)
+})
 
--- src/server.js
+++ src/server.js
@@ -1,19 +1,28 @@
-//Importa dependencia
-const express = require('express')
-const path = require('path')
+//Importar dependencia
+const express = require('express');
+const path = require('path');
+const pages = require('./pages.js');
+
 
 //Iniciando o express
-const server = express()
+const server = express();
+
 server
-//Utilizando os arquivos estaticos
+
+//Utilizando arquivos estáticos
 .use(express.static('public'))
 
-//Criar uma rota
-.get('/', (request, response) =>{
+//configurar template engine
+.set('views', path.join(__dirname, "views"))
+.set('view engine', 'hbs')
 
-  return response.sendFile(path.join(__dirname + 'views', 'index.html'))
-})
-
+//Rotas da aplicação
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
+  
 
 //Ligar o servidor
-server.listen(5000)
+server.listen(5500)
+
