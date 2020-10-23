7fda4bb9057768efa44351bcc3ccb39e68f3aadf
Flávio Félix <fau_felix30@hotmail.com>
2020-10-23 16:50:00 -0300

    Revisando e consertando os bugs pra depois fazer uns updates

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -1,154 +1,73 @@
+//create map
+const map = L.map("mapid").setView([-27.2109325, -49.6448719], 15);
 
-
-//Parte do mapa aqui 
-const map = L.map('mapid').setView([-23.5762796,-46.6665123], 16);
-
-// Criando e adicionando tilelayer
-// Mapa grauíto disponível
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',)
- .addTo(map)
-
-// criando icone 
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 const icon = L.icon({
-    //Peagando a imagem do ícone
-    iconUrl: "/images/map-marker.svg",
-    // Tamanho do ícone 
-    iconSize: [58, 68],
-    //Aonde ele vai estar
-    iconAnchor: [29,68],
-    //Para o popup ficar ao lado 
-    popupAnchor: [170, 2]
-})
-
-// Criando marcador
+  iconUrl: "/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+});
 
 let marker;
 
+map.on("click", (event) => {
+  const lat = event.latlng.lat;
+  const lng = event.latlng.lng;
 
-//Criando e adicionando o mapa 
-map.on('click', (event) =>{
+  document.querySelector("[name=lat]").value = lat;
+  document.querySelector("[name=lng]").value = lng;
 
-    //Pegando a latitude e a longitude
-    const lat = event.latlng.lat;
-    const lng = event.latlng.lng;
+  marker && map.removeLayer(marker);
 
-    // Guardar essa informação para uma variável
+  marker = L.marker([lat, lng], { icon }).addTo(map);
+});
 
-    document.querySelector('[name=lat]').value = lat;
-    document.querySelector('[name=lng]').value = lng;
+function addPhotoField() {
+  const container = document.querySelector("#images");
 
-    //remover icone, para não ficar adicionando infinitamente
+  const fieldsContainer = document.querySelectorAll(".new-upload");
 
-    marker && map.removeLayer(marker)
+  const newFieldContainer = fieldsContainer[
+    fieldsContainer.length - 1
+  ].cloneNode(true);
 
-    // Adicionando o icone no mapa 
-    marker = L.marker([lat, lng], { icon })
-    .addTo(map)
+  const input = newFieldContainer.children[0];
 
-} )
+  if (input.value == "") {
+    return;
+  }
 
+  input.value = "";
 
-// Adicionar o campo de fotos
-
-function addPhotoField(){
-    // pegar o container de fotos #images
-    const container = document.querySelector('#images')
-    //pegar container para duplicar .new-image
-
-    const fieldsContainer  = document.querySelectorAll('.new-upload')
-    // realizar um clone da ultima imagem adicionada
-
-    const newFieldContainer = fieldsContainer[fieldsContainer.length-1].cloneNode(true)
-
-    //Se estiver vazio. não adicionar 
-
-    const input = newFieldContainer.children[0]
-
-    if(input.value == ''){
-        return 
-    }
-
-    //Limpar o campo
-    input.value = ''
-
-    //Adicionar o clone no container de #images
-
-    container.appendChild(newFieldContainer)
-
-
-
-
+  container.appendChild(newFieldContainer);
 }
 
-function deleteField(event){
+function deleteField(event) {
+  const span = event.currentTarget;
 
-    const span = event.currentTarget
+  const fieldsContainer = document.querySelectorAll(".new-upload");
 
-    const fieldsContainer  = document.querySelectorAll('.new-upload')
+  if (fieldsContainer.length <= 1) {
+    span.parentNode.children[0].value = "";
+    return;
+  }
 
-    if (fieldsContainer.length <= 1){
-        // Limpar o valor do campo
-        span.parentNode.children[0].value = ''
-
-        return
-        //Return vazio faz com que não seja executada a ação
-
-    }
-
-    // Deletar o valor do campo
-
-    span.parentNode.remove()// Node parent, pega o que esta encima dele 
-
-
+  span.parentNode.remove();
 }
 
-//Troca do botão sim e do não 
+function toggleSelect(event) {
+  document
+    .querySelectorAll(".button-select button")
+    .forEach((button) => button.classList.remove("active"));
 
+  const button = event.currentTarget;
+  button.classList.add("active");
 
-function toggleSelect(event){ 
-
-    //Retirar a classe .active dos botões
-    document.querySelectorAll('.button-select button')
-    //Retirando active com function simplificada
-    .forEach(button => button.classList.remove('active'))
-
-    //Colocar a .class .active no botão selecionado 
-    const button = event.currentTarget
-    button.classList.add('active')
-
-
-    //Atualizar o input hidden com o valor selcionado
-
-    const input = document.querySelector('[name = "open_on_weekends"]')
-
-    // verificar se sim ou não 
-
-    input.value = button.dataset.value 
-
-
+  const input = document.querySelector('[name="open_on_weekends"]');
+  input.value = button.dataset.value;
 }
 
-//Ver se a validação está correta para continuar com aplicação 
-
-function validate(event){
-    const needsLatAndLng = true 
-
-    //Validar se lat e long estão preenchidos
-    if ( needsLatAndLng == true)
-    event.preventDefault()
-    alert("Selecione um ponto no mapa")
+function validade(event) {
+  // event.preventDefault()
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,57 +1,36 @@
+const map = L.map("mapid").setView([-27.2109325, -49.6448719], 15);
 
-//Parte do mapa aqui 
-const map = L.map('mapid').setView([-23.5762796,-46.6665123], 16);
-
-// Criando e adicionando tilelayer
-// Mapa grauíto disponível
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',)
- .addTo(map)
-
-// criando icone 
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 const icon = L.icon({
-    //Peagando a imagem do ícone
-    iconUrl: "/images/map-marker.svg",
-    // Tamanho do ícone 
-    iconSize: [58, 68],
-    //Aonde ele vai estar
-    iconAnchor: [29,68],
-    //Para o popup ficar ao lado 
-    popupAnchor: [170, 2]
-})
+  iconUrl: "/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
+function addMarker({ id, name, lat, lng } = orphanage) {
+  const popup = L.popup({
+    closeButton: false,
+    className: "map-popup",
+    minWidth: 240,
+    minHeight: 240,
+  }).setContent(
+    `${name} <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"> </a>`
+  );
 
-
-function addMarker({id, name, lat, lng}){
-    //Criando o popup
-
-    const popup = L.popup({
-        //Para não aparecer o Button
-        closeButton: false,
-        className: 'map-popup',
-        minWidth: 240,
-        minHeight: 240
-    }).setContent(`${name} <a href="/orphanage?id=${id}" class="choose-orphanage"><img src="/images/arrow-white.svg"> </a>`)
-
-
-
-    // Fazer a sua localização do mapa 
-    L.marker([lat,lng], { icon })
-    .addTo(map)
-    .bindPopup(popup)
+  L.marker([lat, lng], { icon }).addTo(map).bindPopup(popup);
 }
 
-const orphanagesSpan = document.querySelectorAll('.orphanages span')
-orphanagesSpan.forEach( span =>{
-    //Preenchendo as informações para addmarker
-    const orphanage = {
-        id: span.dataset.id,
-        name: span.dataset.name,
-        lat: span.dataset.lat,
-        lng: span.dataset.lng
+const orphanagesSpan = document.querySelectorAll(".orphanages span");
 
-    }
+orphanagesSpan.forEach((span) => {
+  const orphanage = {
+    id: span.dataset.id,
+    name: span.dataset.name,
+    lat: span.dataset.lat,
+    lng: span.dataset.lng,
+  };
 
-    addMarker(orphanage)
-})
-
+  addMarker(orphanage);
+});
